c42db198bb5343b265ee3ca0f64166c31dd34d36
Alexander Witkowski <alexanderwitkowski@gmail.com>
2018-10-11 16:41:03 +0200

    First implementation to get book by isbn - untested

--- BookScanner.OpenLibClient/OpenLibraryClient.cs
+++ BookScanner.OpenLibClient/OpenLibraryClient.cs
@@ -1,10 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace BookScanner.OpenLibClient
 {
     public class OpenLibraryClient
     {
+        private HttpClient _httpClient;
+
+        public OpenLibraryClient()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://openlibrary.org/api/");
+        }
+
+        public async Task<string> GetBookByIsbnAsync(string isbn)
+        {
+            var result = await _httpClient.GetStringAsync($"books?bibkeys=ISBN:{isbn}&format=json").ConfigureAwait(false);
+            return result;
+        }
     }
 }
