ea8a0a2be232a60b3bd992d45b16ff1a934a154f
Alexander Witkowski <alexanderwitkowski@gmail.com>
2018-10-11 17:28:09 +0200

    Changed api function to return book result

--- BookScanner.OpenLibClient/OpenLibraryClient.cs
+++ BookScanner.OpenLibClient/OpenLibraryClient.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -16,11 +17,11 @@ namespace BookScanner.OpenLibClient
             _httpClient.BaseAddress = new Uri("https://openlibrary.org/api/");
         }
 
-        public async Task<string> GetBookByIsbnAsync(string isbn)
+        public async Task<BookResult> GetBookByIsbnAsync(string isbn)
         {
             var result = await _httpClient.GetStringAsync($"books?bibkeys=ISBN:{isbn}&format=json&jscmd=data").ConfigureAwait(false);
             var res = JsonConvert.DeserializeObject<Dictionary<string, BookResult>>(result);
-            return result;
+            return res.FirstOrDefault().Value;
         }
     }
 }
